6d0f345b75ca28911f771b3dcbe204a28311bfa0
BHAGYA-PV <146536866+BHAGYA-PV@users.noreply.github.com>
2025-12-03 00:56:31 +0530

    model creation for file upload

--- ocr_document/models.py
+++ ocr_document/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Document(models.Model):
+    filename = models.CharField(max_length=255)
+    path = models.FileField(upload_to="documents/")
+    uploaded_date = models.DateTimeField(auto_now_add=True)
+    mime_type = models.CharField(max_length=100)
+    ocr_text = models.TextField(blank=True, null=True, db_index=True)
+
+    def __str__(self):
+        return self.filename
